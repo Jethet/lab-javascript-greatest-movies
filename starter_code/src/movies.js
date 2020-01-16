4cb4b3c0fde1a7292238c8635bad776f8e709d65
Jethet <henriette.hettinga@gmail.com>
2020-01-16 18:44:43 +0100

    Two and three

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,7 +1,6 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-//const moviesTwo = [...movies];
 
 function orderByYear(arr){
     const arr2 = [...arr];   // copy of original array
@@ -19,7 +18,33 @@ function orderByYear(arr){
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function dramaSpielberg(arr) {
+    const arr2 = arr.filter(function(e){
+        return e.genre.includes("Drama") && e.director === "Steven Spielberg";
+    });
+    return arr2.length;
+}
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(arr) {
+    const arr2 = [...arr]; 
+    const arrSorted = arr2.sort(function(a,b){
+        if (a.title < b.title){
+            return -1;
+        } else if (a.title > b.title){
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    const arr20 = arrSorted.slice(0,20);
+
+    const titles20 = arr20.map(function(movie){
+        return movie.title;
+    });
+    return titles20;
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
